9041931f29b9df16120dea1c87e350463d5ab3d8
KnowingZzaduke <josex.developer@gmail.com>
2023-03-14 10:18:36 -0500

    Agregando el menú de navegación

--- src/components/principal/Navbar.jsx
+++ src/components/principal/Navbar.jsx
@@ -1,24 +1,35 @@
-import Logo from '/img/Dysam.jpg';
+import { FaBars, FaUserCheck, FaInfoCircle, FaPhone } from "react-icons/fa";
+import { useState } from "react";
+import Logo from "/img/Dysam.jpg";
 export function Navbar() {
+  const [active, setActive] = useState(false);
+  function handleClick(){
+    setActive(!active)
+  }
   return (
     <div className="content_navbar">
       <nav className="navbar">
-      <div className="content_logo">
-        <img src={Logo} className='logo'>
-        </img>
-      </div>
-        <div className="content_enlaces">
+        <div className="content_logo">
+          <img src={Logo} className="logo"></img>
+        </div>
+        <div className={`content_enlaces-n ${active ? "content_enlaces-d" : ""}`}>
           <ul>
             <li>
-                <a>Iniciar sesión</a>
+              <FaUserCheck />
+              <a>Iniciar sesión</a>
             </li>
             <li>
-                <a>Sobre nosotros</a>
+              <FaInfoCircle />
+              <a>Sobre nosotros</a>
             </li>
             <li>
-                <a>Contacto</a>
+              <FaPhone />
+              <a>Contacto</a>
             </li>
           </ul>
+        </div>
+        <div className="content_toggle-abrir">
+          <FaBars onClick={handleClick}/>
         </div>
       </nav>
     </div>
