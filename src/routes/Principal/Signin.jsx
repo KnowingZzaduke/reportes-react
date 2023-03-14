d6ee1f7ac990d9e1b27d3cda3e86484c96699637
KnowingZzaduke <josex.developer@gmail.com>
2023-03-14 15:08:40 -0500

    Agregando el formulario de iniciar sesión

--- src/routes/Principal/Signin.jsx
+++ src/routes/Principal/Signin.jsx
@@ -4,17 +4,25 @@ export function Signin() {
   return (
     <div className="content_formulario">
       <form>
-        <div className="content_logo">
-          <img src={Logo} />
-        </div>
-        <fieldset>
-          <input type="text" placeholder="Usuario" />
-          <input type="password" placeholder="Contraseña" />
-        </fieldset>
-        <div className="content_boton">
-          <Link>
-            <button>Enviar</button>
-          </Link>
+        <div className="content_all-form">
+          <div className="content_logo">
+            <img src={Logo} />
+          </div>
+          <fieldset>
+            <div className="content_input">
+              <label>Usuario</label>
+              <input type="text" placeholder="Usuario" />
+            </div>
+            <div className="content_input">
+              <label>Contraseña</label>
+              <input type="password" placeholder="Contraseña" />
+            </div>
+          </fieldset>
+          <div className="content_boton">
+            <Link>
+              <button>Enviar</button>
+            </Link>
+          </div>
         </div>
       </form>
     </div>
